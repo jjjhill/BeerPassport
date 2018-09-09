faee9d65bcf3a522960f9751bb6f161f5f0115bd
Josh Hill <jjjhill@uwaterloo.ca>
2018-09-09 18:09:03 -0400

    add beer logos to beer list entries, allowing for customizable beer colors in the future

--- components/BeerListEntry.js
+++ components/BeerListEntry.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class BreweryListEntry extends React.Component {
+
+    getBeerColor() {
+        return 'gold';
+    }
   render() {
     return (
-        <View style={{padding: 20, height:100, backgroundColor:'white', 
+        <View style={{padding: 10, height:100, backgroundColor:'white', 
           borderBottomColor: 'silver', borderBottomWidth: 1,
-          flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+          flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+            <Image resizeMode='contain' style={{width:70, height:70, marginRight:10, zIndex:1}} source={require('../Beer-Mug.png')}/>
+            <View style={{ width:32, height:46, marginBottom:2, backgroundColor: this.getBeerColor(), position:'absolute', left:22, zIndex:0 }} />
             <View>
                 <Text style={{textAlign: 'left', fontSize: 16}}>
                     {this.props.item.breweryName}
@@ -18,15 +24,15 @@ export default class BreweryListEntry extends React.Component {
                     {this.props.item.type}
                 </Text>
             </View>
-            <View style={{height: 80, width: 80, justifyContent: "space-between"}}>
-                <Text style={{textAlign: 'center', fontSize: 16}}>
-                        {this.props.item.abv}% ABV
+            <View style={{height: 80, width: 80, justifyContent: "space-between", alignItems:'flex-end', position: 'absolute', right: 10}}>
+                <Text style={{fontSize: 16}}>
+                    {this.props.item.abv}% ABV
                 </Text>
-                <Text style={{textAlign: 'center', fontSize: 16}}>
-                        {this.props.item.ibu} IBU
+                <Text style={{fontSize: 16}}>
+                    {this.props.item.ibu} IBU
                 </Text>
-                <Text style={{textAlign: 'center', fontSize: 16}}>
-                        {this.props.item.rating} / 10
+                <Text style={{fontSize: 16}}>
+                    {this.props.item.rating} / 10
                 </Text>
             </View>
         </View>
